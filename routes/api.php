1b7c6aa6bd5a2e5289346038fc60f42c3243a189
Ahsan <ahsanan@outlook.com>
2026-03-10 22:29:05 +0600

    user registration complete

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::post('/users', [UserController::class, 'store']);
