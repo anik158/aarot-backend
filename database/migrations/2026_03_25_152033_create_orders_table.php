0617a021a6ab5633493efd59e6322632e479ad33
Ahsan <ahsanan@outlook.com>
2026-03-25 23:27:28 +0600

    Order template is set

--- database/migrations/2026_03_25_152033_create_orders_table.php
+++ database/migrations/2026_03_25_152033_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
             $table->string('order_number')->unique();
 
             $table->decimal('subtotal', 10, 4)->default(0);
